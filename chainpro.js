0c2c4c26f1eb21144f72f12abacd50b15a9f9da0
quiquee <enrique.melero@gmail.com>
2021-11-19 14:03:20 +0100

    Event watching, several UI changes. Export working and import half done

--- chainpro.js
+++ chainpro.js
@@ -10,7 +10,10 @@ const featherIconsConst = Object.freeze({
   LEFT: "<i class='feather icon-arrow-left'></i>",
   DELETE: "<i class='feather icon-delete'></i>",
   EYE: "<i class='feather icon-eye'></i>",
+  FOLDER: "<i class='feather icon-folder'></i>",
+  DOLLAR_SIGN: "<i class='feather dollar-sign'></i>",
   BITCOIN: "&#x20BF;",
+
 });
 
 
@@ -29,13 +32,13 @@ var data = {
     ethAddress: "0e64837ed56479314144af5bba4192bb161650fe",
     path: 'm/0/1',
     status: 'pending'
-  },{
+  }, {
     name: "CTO Office",
     ethAddress: "86fa4e294747a0436826703d1b384440f4753cb0",
     balance: null,
     path: 'm/0/2',
     status: 'pending'
-  },{
+  }, {
     name: "Marketing Department",
     ethAddress: "09d110c81a6453df7fee4417683d70163235fc84",
     balance: null,
@@ -68,22 +71,20 @@ Vue.component('item', {
       return this.model.children &&
         this.model.children.length
     },
-    
+
     shortAddress: function () {
-      return "0x" + this.model.ethAddress.substring(0, 7) + "..." ;
+      return "0x" + this.model.ethAddress.substring(0, 7) + "...";
     },
 
     balanceString: function () {
-      return ( this.model.balance * 100000).toFixed(2) + "$";
+      return (this.model.balance * 1000).toFixed(2) + "$";
     }
   },
   created: async function () {
     window.web3 = new Web3(web3.currentProvider);
     console.log("Address of " + this.model.name + " is " + this.model.ethAddress);
-    const response = await web3.eth.getBalance(this.model.ethAddress);
-    console.log("Response: " + response);
-    this.model.balance = web3.utils.fromWei(response, 'ether');
-    this.model.status = null;
+    this.updateBalance();
+
   },
 
   mounted: async function () {
@@ -97,6 +98,16 @@ Vue.component('item', {
             var accounts = ethereum.request({ method: 'eth_requestAccounts' });
             accounts.then(function (data) {
               document.getElementById("address").innerText = data[0];
+              var subscription = web3.eth.subscribe('newBlockHeaders',
+                function (error, result) {
+                  if (!error) {
+                    console.log("New headers " + result);
+                    demo.$children[0].updateChildBalances();
+                  } else {
+                    console.log("New headers. error " + error);
+
+                  }
+                });
 
             });
           } else {
@@ -129,6 +140,24 @@ Vue.component('item', {
         this.open = true
       }
     },
+
+    updateBalance: async function () {
+      console.log("Called updateBalance()");
+      this.model.status = 'Updating...';
+      const response = await web3.eth.getBalance(this.model.ethAddress);
+      console.log("Response: 0x" + this.model.ethAddress + " > " + response);
+      this.model.balance = web3.utils.fromWei(response, 'ether');
+      this.model.status = null;
+    },
+
+    updateChildBalances: function () {
+      this.$children.forEach(element => {
+        console.log("El name: " + element.name);
+        element.updateBalance();
+      });
+      this.updateBalance();
+    },
+
 
     addChild: function () {
       var newpath = this.model.path + "/" + (this.model.children.length + 1);
@@ -173,4 +202,41 @@ function toggleLog() {
   } else {
     document.getElementById('msglog').style.display = 'block';
   }
+}
+
+function downloadTree() {
+
+  var a = document.createElement('a');
+  let filename = "export.json";
+  let contentType = "application/json;charset=utf-8;";
+  a.download = filename;
+  a.href = 'data:' + contentType + ',' +
+    encodeURIComponent(JSON.stringify(demo.treeData));
+  a.target = '_blank';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+function chooseFile() {  
+  document.getElementById('import').style.display='block';
+}
+
+function importFile() {
+  var files = document.getElementById('selectFiles').files;
+  
+  console.log(files);
+  if (files.length <= 0) {
+    return false;
+  }
+  var fr = new FileReader();
+
+  fr.onload = function (e) {
+    console.log(e);
+    var result = JSON.parse(e.target.result);
+    var formatted = JSON.stringify(result, null, 2);
+    document.getElementById('result').value = formatted;
+  }
+  fr.readAsText(files.item(0));
+  
 }
